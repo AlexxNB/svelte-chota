31a522a008d71a918b37c556b6cd307f3e7b66ce
AlexxNB <iam@alexxnb.ru>
2019-09-28 23:57:45 +0300

    fixed import highlight.js

--- docs_src/src/cmp/Snippet.svelte
+++ docs_src/src/cmp/Snippet.svelte
@@ -1,18 +1,39 @@
 <script>
-import hljs from 'highlight.js';
+import hljs from 'highlight.js/lib/highlight';
+import xml_lang from 'highlight.js/lib/languages/xml';
+hljs.registerLanguage('xml', xml_lang);
+
+import {Row,Col} from './../../../index';
 
 export let code = '';
+
+console.log($$props.$$slots.default);
 
 $: hl_code = hljs.highlight('html',code.trim());
 </script>
 
-<div>
-	<div class="result"><slot></slot></div>
-	<div class="hljs">
+
+<Row>
+	<Col size="7">
+		<div class="result">
+			<slot></slot>
+		</div>
+	</Col>
+	<Col size="5">
+		<pre class="hljs">
 		{@html hl_code.value}
-	</div>
-</div>
+		</pre>
+	</Col>
+</Row>
+
+
 
 <style>
+.hljs{
+	border-radius: 5px;
+	font-size: 1.2rem;
+	line-height: 1.2rem;
+}
+
 
 </style>
