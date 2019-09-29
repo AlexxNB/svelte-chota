c8f5ea5efe9fa22926b9e6f87ff25a6e397daee9
AlexxNB <iam@alexxnb.ru>
2019-09-29 12:55:09 +0300

    Added button coponent

---
+++ cmp/Button.svelte
@@ -0,0 +1,100 @@
+<script>
+    import {getClassAttr} from './utils';
+    import Icon from 'mdi-svelte';
+
+    export let outline = null;
+    export let primary = null;
+    export let secondary = null;
+    export let dark = null;
+    export let error = null;
+    export let success = null;
+    export let clear = null;
+    export let loading = null;
+    export let icon = null;
+    export let iconRight = null;
+
+    export let disabled = false;
+
+    const hasSlot = ($$props.$$slots !== undefined);
+
+    $: clIcon = ( (icon !== null || iconRight !== null) && hasSlot);
+    $: clIcononly = (icon !== null && !hasSlot);
+
+    $: classAttr = getClassAttr('button',$$props.class);
+</script>
+
+
+<button  
+    class="{classAttr}"
+
+    class:outline
+    class:primary
+    class:secondary
+    class:dark
+    class:error
+    class:success
+    class:clear
+    class:loading
+    
+    class:icon={clIcon}
+    class:icon-only={clIcononly}
+
+    {disabled}
+
+    on:*
+>
+{#if icon} <span class="lefticon"> <Icon path={icon} size="1.4"/> </span>{/if}
+<slot></slot>
+{#if iconRight} <span class="righticon"> <Icon path={iconRight} size="1.4"/> </span>{/if}
+</button>
+
+
+<style>
+@keyframes loading {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+}
+
+.loading {
+    color: transparent !important;
+    min-height: .8rem;
+    pointer-events: none;
+    position: relative;
+}
+
+.loading::after {
+    animation: loading 500ms infinite linear;
+    border: .2rem solid #FFFFFF;
+    border-radius: 50%;
+    border-right-color: transparent;
+    border-top-color: transparent;
+    content: "";
+    display: block;
+    height: .8rem;
+    left: 50%;
+    margin-left: -.4rem;
+    margin-top: -.4rem;
+    position: absolute;
+    top: 50%;
+    width: .8rem;
+    z-index: 1;
+}
+
+
+.icon > .lefticon{
+  margin: -10px 10px -10px 0px;
+}
+
+.icon > .righticon{
+  margin: -10px 0px -10px 10px;
+}
+
+
+.icon-only{
+  padding:.75rem 1rem;
+}
+</style>
---
+++ docs_src/src/pages/buttons.svelte
@@ -0,0 +1,78 @@
+<script>
+    import Snippet from './../cmp/Snippet.svelte';
+    import {Button} from './../../../index';
+
+    import { mdiDelete,mdiAccountPlus,mdiSend } from '@mdi/js'; 
+</script>
+
+<h1>Buttons</h1>
+
+<h3>Default buttons</h3>
+
+<Snippet code={`
+<Button>Default</Button>
+<Button primary>Primary</Button>
+<Button secondary>Secondary</Button>
+<Button dark>Dark</Button>
+<Button error>Error</Button>
+<Button success>Success</Button>
+<Button loading>Loading</Button>
+<Button disabled>Disabled</Button>
+`}>
+    <Button>Default</Button>
+    <Button primary>Primary</Button>
+    <Button secondary>Secondary</Button>
+    <Button dark>Dark</Button>
+    <Button error>Error</Button>
+    <Button success>Success</Button>
+</Snippet>
+
+<h3>Outline buttons</h3>
+
+<Snippet code={`
+<Button outline>Default</Button>
+<Button primary outline>Primary</Button>
+<Button secondary outline>Secondary</Button>
+<Button dark outline>Dark</Button>
+<Button clear>Clear</Button>
+`}>
+    <Button outline>Default</Button>
+    <Button primary outline>Primary</Button>
+    <Button secondary outline>Secondary</Button>
+    <Button dark outline>Dark</Button>
+    <Button clear>Clear</Button>
+</Snippet>
+
+
+<h3>Icon buttons</h3>
+
+<p>Properties <code>icon</code> or <code>iconRight</code> accepts any SVG path for icon. 
+As example, you can use <a target="_blank"  href="https://www.npmjs.com/package/@mdi/js">@mdi/js</a> package. But you are free to use any other.</p>
+
+<Snippet code={`
+<script>
+    import { mdiDelete,mdiAccountPlus,mdiSend } from '@mdi/js';
+</script>
+
+<Button primary icon={mdiAccountPlus}>Add User</Button>
+<Button outline primary iconRight={mdiSend}>Submit</Button>
+<Button error icon={mdiDelete} />
+`}>
+    <Button primary icon={mdiAccountPlus}>Add User</Button>
+    <Button outline primary iconRight={mdiSend}>Submit</Button>
+    <Button error icon={mdiDelete} />
+</Snippet>
+
+<h3>States</h3>
+
+<Snippet code={`
+<Button loading>Loading</Button>
+<Button loading error>Loading</Button>
+<Button disabled>Disabled</Button>
+<Button primary disabled>Disabled</Button>
+`}>
+    <Button loading>Loading</Button>
+    <Button loading error>Loading</Button>
+    <Button disabled>Disabled</Button>
+    <Button primary disabled>Disabled</Button>
+</Snippet>
