96447f8641db5a83ffae5d051865ca6d8223fa3a
AlexxNB <iam@alexxnb.ru>
2019-09-30 14:54:44 +0300

    refact

--- docs_src/src/cmp/Docs.svelte
+++ docs_src/src/cmp/Docs.svelte
@@ -2,15 +2,11 @@
 	import { onMount } from 'svelte';
     import {getComponents} from './../routes';
     
-
     export let section = 'top';
-
     let scroll = 0;
-
 
     function scrollToAnchor(slug) {
         const anchor = document.getElementById(`anchor_${slug}`);
-        console.log(slug);
         if(anchor) {
             const offset = anchor.getBoundingClientRect();
             scroll += offset.top - 45;
