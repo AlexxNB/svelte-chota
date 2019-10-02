1bc82fb5defd93ec03392309335db745d21a19a6
AlexxNB <iam@alexxnb.ru>
2019-10-02 18:42:52 +0300

    setup modules bundling

---
+++ cmp/index.js
@@ -0,0 +1,31 @@
+import Nav from './Nav.svelte';
+import Row from './Row.svelte';
+import Col from './Col.svelte';
+import Button from './Button.svelte';
+import Tabs from './Tabs.svelte';
+import Tab from './Tab.svelte';
+import Card from './Card.svelte';
+import Tag from './Tag.svelte';
+import Details from './Details.svelte';
+import Input from './Input.svelte';
+import Field from './Field.svelte';
+import Radio from './Radio.svelte';
+import Checkbox from './Checkbox.svelte';
+import Modal from './Modal.svelte';
+import Icon from './Icon.svelte';
+
+export {
+    Nav,
+    Row, Col,
+    Button,
+    Tabs, Tab,
+    Card,
+    Tag,
+    Details,
+    Input,
+    Field,
+    Radio,
+    Checkbox,
+    Modal,
+    Icon,
+}
