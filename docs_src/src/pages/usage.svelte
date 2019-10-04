575f19f17464df9aae4deeb79a312285dbf79062
AlexxNB <iam@alexxnb.ru>
2019-10-04 17:16:41 +0300

    added events halders description

--- docs_src/src/pages/usage.svelte
+++ docs_src/src/pages/usage.svelte
@@ -1,5 +1,8 @@
 <script>
     import Snippet from './../cmp/Snippet.svelte';
+    import {Button} from './../../../cmp/index';
+
+    let button_text = 'Hover me';
 </script>
 <h1>Usage</h1>
 
@@ -13,5 +16,29 @@
 `}>
 
 <p>Just import necessary components from the <code>svelte-chota</code> package in your components</p>
+
+</Snippet>
+
+
+<h3>Events handlers</h3>
+
+<p>You can use any <code>`on:eventname`</code> directive with almost any components.</p>
+
+<Snippet code={`
+<script>
+    import {Button} from 'svelte-chota';
+    let button_text = 'Hover me';
+</script>
+
+<Button 
+    on:mouseenter={ e => button_text="Don't touch me!" }
+    on:mouseleave={ e => button_text="Ok, hover me again" }
+>{button_text}</Button>
+`}>
+
+<Button 
+    on:mouseenter={ e => button_text="Don't touch me!" }
+    on:mouseleave={ e => button_text="Ok, hover me again" }
+>{button_text}</Button>
 
 </Snippet>
