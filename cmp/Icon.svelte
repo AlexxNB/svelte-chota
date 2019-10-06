bb0888f5e54bcb55d21003bc6515076ba7b22de2
AlexxNB <iam@alexxnb.ru>
2019-10-06 10:41:52 +0300

    refact

--- cmp/Icon.svelte
+++ cmp/Icon.svelte
@@ -29,70 +29,63 @@
 		url = use = false;
 	}
 
-  // SPIN properties
-  $: inverse = (typeof spin !== "boolean" && spin < 0) ? true : false;
-  $: spintime = Math.abs(spin === true ? 2 : spin);
-  $: spinCW = (!!spin && !inverse);
-  $: spinCCW = (!!spin && inverse);
+	// SPIN properties
+	$: inverse = (typeof spin !== "boolean" && spin < 0) ? true : false;
+	$: spintime = Math.abs(spin === true ? 2 : spin);
+	$: spinCW = (!!spin && !inverse);
+	$: spinCCW = (!!spin && inverse);
 
-  // size
-  if(Number(size)) size = Number(size);
-  
-  const getStyles = () => {
-      const transform = [];
-      const styles = [];
-      if (size !== null) {
-          const width = (typeof size === "string") ? size : `${size * 1.5}rem`;
-          styles.push(['width',width]);
-          styles.push(['height',width]);
-      }
-      styles.push( ['fill', (color !== null) ? color: 'currentColor'] );
-      if (flipH) {
-        transform.push("scaleX(-1)");
-      }
-      if (flipV) {
-        transform.push("scaleY(-1)");
-      }
-      if (rotate != 0) {
-        transform.push(`rotate(${rotate}deg)`);
-  
-      }
-      if(transform.length > 0) {
-        styles.push( ['transform', transform.join(' ')] );
-        styles.push( ['transform-origin', 'center'] );
-      }
-      return styles.reduce((cur,item)=>{
-        return `${cur} ${item[0]}:${item[1]};`;
-      },'');
-  }
-  $: style = getStyles(size,color,flipH,flipV,rotate);
+	// size
+	if(Number(size)) size = Number(size);
+	
+	// styles
+	const getStyles = () => {
+		const transform = [];
+		const styles = [];
+		if (size !== null) {
+			const width = (typeof size === "string") ? size : `${size * 1.5}rem`;
+			styles.push(['width',width]);
+			styles.push(['height',width]);
+		}
+		styles.push( ['fill', (color !== null) ? color: 'currentColor'] );
+		if (flipH) {
+			transform.push("scaleX(-1)");
+		}
+		if (flipV) {
+			transform.push("scaleY(-1)");
+		}
+		if (rotate != 0) {
+			transform.push(`rotate(${rotate}deg)`);
+	
+		}
+		if(transform.length > 0) {
+			styles.push( ['transform', transform.join(' ')] );
+			styles.push( ['transform-origin', 'center'] );
+		}
+		return styles.reduce((cur,item)=>{
+			return `${cur} ${item[0]}:${item[1]};`;
+		},'');
+	}
+	$: style = getStyles(size,color,flipH,flipV,rotate);
+	$: aniStyle = (!!spin) ? `animation-duration: ${spintime}s` : undefined;
 </script>
 
 {#if url}
 	<span {style} use:events use:attrs={$$props}>
-		<img src="{url}" alt="" width="100%" height="100%" 
-			class:spinCW class:spinCCW 
-			style={(!!spin) ? `animation-duration: ${spintime}s` : undefined}
-		/>
+		<img src="{url}" alt="" width="100%" height="100%" class:spinCW class:spinCCW style={aniStyle} />
 	</span>
 {:else if use}
 	<svg viewBox="0 0 24 24" {style} use:events use:attrs={$$props}>
-		<use xlink:href={use}
-			class:spinCW class:spinCCW 
-			style={(!!spin) ? `animation-duration: ${spintime}s` : undefined}
-		></use>
+		<use xlink:href={use} class:spinCW class:spinCCW style={aniStyle}></use>
 	</svg>
 {:else}
 	<svg viewBox="0 0 24 24" {style} use:events use:attrs={$$props}>
 	{#if spin !== false}
-		<g 
-			class:spinCW class:spinCCW 
-			style={(!!spin) ? `animation-duration: ${spintime}s` : undefined}
-		>
+		<g class:spinCW class:spinCCW style={aniStyle}>
 			<path d={path}></path>
 		</g>
 	{:else}
-	<path d={path}></path>
+		<path d={path}></path>
 	{/if} 
 	</svg>
 {/if}
