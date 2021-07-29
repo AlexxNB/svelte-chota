3f8a7c972dcf589a478e435106f182698447cbce
AlexxNB <iam@alexxnb.ru>
2021-07-29 14:23:22 +0300

    refactor: :art: Use span element for tabs instead anchors

--- cmp/Tab.svelte
+++ cmp/Tab.svelte
@@ -12,7 +12,6 @@
     $: active = ($active_tab === tabid || $active_tab === id);
 </script>
 
-<!--TODO: write issue to chota to replace a by span-->
-<a href="/" class:active use:events {...$$restProps} on:click|preventDefault={()=>active_tab.set( (tabid === false) ? id : tabid)}>
+<span class:active use:events {...$$restProps} on:click|preventDefault={()=>active_tab.set( (tabid === false) ? id : tabid)}>
   <slot></slot>
-</a>
+</span>
